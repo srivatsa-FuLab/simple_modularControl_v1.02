386da7ab65c57d57e24b4800d1c2b29d8ec4fc9b
Srivatsa Chakravarthi <srivatsa@uw.edu>
2021-02-13 23:03:30 -0800

    Update pflipConfig.m

--- mcInstruments/@mciPFlip/pflipConfig.m
+++ mcInstruments/@mciPFlip/pflipConfig.m
@@ -2,7 +2,7 @@
 %Here the flip mirror connected via DAQ is combined with the GPIB Newport powermeter
 
 function config = pflipConfig()
-	config.class = 'mciPower';
+	config.class = 'mciPFlip';
 	
 	config.name = 'Powermeter';
 	
@@ -13,7 +13,7 @@ function config = pflipConfig()
 	flipConfig.name = 	'Flip Mirror';
 	config.flip =       mcaDAQ(flipConfig);
 
-	config.kind.kind =          'mciPower';
+	config.kind.kind =          'mciPFlip';
 	config.kind.name =          'Interjecting Powermeter';
 	config.kind.extUnits =      config.power.config.kind.extUnits; % 'External' units.
 	config.kind.shouldNormalize = false;                    % If this variable is flagged, the measurement is subtracted from the previous and is divided by the time spent on a pixel. Not that this is done outside the measurement currently in mcData (individual calls to .measure() will not have this behavior currently)
